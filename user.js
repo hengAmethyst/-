2b9fda85c556667f3e8c135d3c271a2d5ce2ad67
jiangheng <jianghengamethyst@163.com>
2018-07-12 10:28:13 +0800

    用户登录注册接口完成

--- user.js
+++ user.js
@@ -1,26 +1,32 @@
-define(['jquery'], function($) {
+define(['base','com_v2','md5'], function($,com,md5) {
     let user = () => {
-        const Api = {
+        let Api = {
             register:'/userCenter/user/regist',
             login:'/userCenter/user/login',
             imgCode:'/userCenter/kaptcha.jpg',
             smsCode:'/userCenter/user/sendSMS',
             formToken:'/feapi/users/formToken',
             voiceCode:'/userCenter/user/sendVoice',
+            checkKaptcha:'/userCenter/kaptchaCode/checkKaptcha'
         }
-        const rcode = 200000
+        let rcode = 200000
 
         let formToken_reg = ''
 
         let formToken_log = ''
 
-        const getValue = (dom) => {
-            let value = null
-            // 获取电话
+        var userData = new Object()
+        var userData_1 = new Object()
+
+        /**
+         * 
+         * @param {*} dom input对应的dom
+         * @param {*} key userData对象中对应的key值
+         */
+        let getValue = (data,dom,key) => {
             $(dom).blur(() => {
-                value = $(dom).value()
+                data[key] = $(dom).val()
             })
-            return value
         }
 
         let getFormToken = () => {
@@ -46,14 +52,9 @@ define(['jquery'], function($) {
             })
             return pro
         }
-        // 初始获取formToken
-        getFormToken().then(data => {formToken_reg = data},data => {formToken_reg = data})
-        getFormToken().then(data => {formToken_log = data},data => {formToken_reg = data})
-
-
 
         // 注册方法
-        let registerWay = (param) => {
+        let register = (param) => {
             /**
              * @param {object}
              * -  phone
@@ -62,11 +63,32 @@ define(['jquery'], function($) {
              * -  smsCode
              * -  submit
              * -  voiceCode
+             * -  imgCodeButton
+             * -  smsCodeButton
              */
+
+            
+            // 判断参数并提示
+            (() => {
+                let mustParam = ['phone','password','imgCode','smsCode','submit','imgCodeButton','smsCodeButton','warn']
+                mustParam.forEach(item => {
+                    let mark = false
+                    for(key in param){
+                        if(key == item){
+                            mark = true
+                        }
+                    }
+                    if(!mark){
+                        console.error(`请填写注册${item}的dom`)
+                    }
+                })
+            })(param)
+
+
             
             // 获取图片验证码
             let imgCode = (formToken) => {
-                $(param.imgCode).attr('src',`${Api.imgCode}?formtoken=${formToken}&v=Math.random()`)
+                $(param.imgCodeButton).css('background-image',`url(${Api.imgCode}?formtoken=${formToken}&t=${Math.random()})`)
             }
 
             // 获取短信验证码
@@ -94,10 +116,10 @@ define(['jquery'], function($) {
                         data: JSON.stringify(getMsgData),
                         success: (res) => {
                             if(res.code == rcode && res.data.code == 0){
-                                resolve(res.data.code)
+                                resolve(res.data)
                             }
                             else{
-                                reject(res)
+                                reject(res.data)
                             }
                         },
                         error: (res) => {
@@ -147,9 +169,15 @@ define(['jquery'], function($) {
             }
 
             // 注册
-            let register = (userData) => {
+            let registerWay = (userData) => {
                 let formData = {
-                    data: userData
+                    data: {
+                        userName: userData.phone,
+                        phone:    userData.phone,
+                        password: $.md5($.md5(userData.password)),
+                        code:     userData.smsCode,
+                        inviterIdentity: null
+                    }
                 }
                 let pro = new Promise((resolve,reject) => {
                     com.ajax({
@@ -177,17 +205,18 @@ define(['jquery'], function($) {
                 })
                 return pro
             }
-
-            // 运行逻辑
-            var userData = new Object()
+            // 初始获取formToken
+            getFormToken().then(data => {formToken_reg = data},data => {formToken_reg = data})
+            // 初始化展示图片验证码
+            imgCode(formToken_reg)
             // 获取电话
-            userData.phone    = getValue(param.phone)
+            getValue(userData,param.phone,'phone')
             // 获取密码
-            userData.password = getValue(param.password)
+            getValue(userData,param.password,'password')
             // 获取图片验证码
-            userData.imgCode  = getValue(param.imgCode)
+            getValue(param.imgCode,'imgCode')
             // 获取短信验证码
-            userData.smsCode  = getValue(param.smsCode)
+            getValue(userData,param.smsCode,'smsCode')
             /**
              * 校验函数
              */
@@ -208,7 +237,7 @@ define(['jquery'], function($) {
              * 点击事件
              */
             // 点击获取图片验证码
-            $(param.imgCode).click(() => {
+            $(param.imgCodeButton).click(() => {
                 getFormToken().then(
                     formToken => {
                         imgCode(formToken)
@@ -219,64 +248,249 @@ define(['jquery'], function($) {
                 )
             })
             // 点击获取短信验证码
-            $(param.smsCode).click(() => {
+            $(param.smsCodeButton).click(() => {
                 // 校验手机号
                 if(checkPhone(userData.phone)){
-                    $(param.warn).text('请输入正确的手机号')
+                    $(param.warn).removeClass('dishidden').text('请输入正确的手机号')
                     userData.phone = null
-                    $(param.phone).value('')
                     return
                 }
                 // 校验密码
                 if(checkPassword(userData.password)){
-                    $(param.warn).text('密码由大写字母、小写字母、数字及特殊字符至少三种组成')
+                    $(param.warn).removeClass('dishidden').text('密码应为6-16位数字与字母组合')
                     return
                 }
                 // 获取短信
                 if(userData.imgCode){
                     smsCode().then(
                         (res) => {
-                            $(param.smsCode).props('disable',true)// 按钮设为  不可点
+                            $(param.smsCodeButton).prop('disabled',true)// 按钮设为  不可点
+                            $(param.warn).removeClass('dishidden').text(res.message)
                             let startTime = 60
                             let timer = setInterval(() => {
                                 startTime--
-                                $(param.smsCode).text(`${startTime}s`)
+                                $(param.smsCodeButton).text(`${startTime}s`)
                                 if(startTime == 0){
                                     clearInterval(timer)
-                                    $(param.smsCode).props('disable',false)// 按钮设为  可点
-                                    $(param.smsCode).text('重新获取')
+                                    $(param.smsCodeButton).prop('disabled',false)// 按钮设为  可点
+                                    $(param.smsCodeButton).text('重新获取')
+                                    getFormToken().then(// 重新获取图片验证码
+                                        formToken => {
+                                            imgCode(formToken)
+                                        },
+                                        formToken => {
+                                            imgCode(formToken)
+                                        }
+                                    )
                                 }
                             },1000)
                         },
                         (error) => {
-                            $(param.warn).text(error.message)
+                            $(param.warn).removeClass('dishidden').text(error.message)
                         }
                     )
                 }
                 else{
-                    $(param.warn).text('图片验证码不能为空')
+                    $(param.warn).removeClass('dishidden').text('图片验证码不能为空')
                 }
             })
+            // 点击获取语音验证码
+            if(param.voiceCode){
+                $(param.voiceCode).click(() => {
+                    voiceCode().then((res) => {
+                        $(param.warn).removeClass('dishidden').text(res.message)
+                    })
+                })
+            }
             // 点击提交用户数据
             $(param.submit).click(() => {
-                register(userData).then(
+                registerWay(userData).then(
                     (res) => {
-                        $.cookie('userToken', res.data , { path: '/', expires: 15 })// 存储cookie
+                        function setCookie(cname, cvalue, exdays) {
+                            var d = new Date();
+                            d.setTime(d.getTime() + (exdays*24*60*60*1000));
+                            var expires = "expires="+d.toUTCString();
+                            document.cookie = cname + "=" + cvalue + "; " + expires;
+                        }
+                        setCookie('userToken',res.data,1)//把tkoen存起来
+                        $(param.warn).removeClass('dishidden').text('注册成功')
                         // 成功回调
                         if(param.success && typeof(param.success) == 'function'){
-                            param.success()
+                            param.success(res)
                         }
                     },
                     (error) => {
-                        $(param.warn).text(error.message)
+                        $(param.warn).removeClass('dishidden').text(error.message)
                         // 失败回调
                         if(param.fail && typeof(param.fail) == 'function'){
-                            param.fail()
+                            param.fail(error)
                         }
                     }
                 )
             })
         }
+        let login = (param) => {
+            /**
+             * @param {object}
+             * -  phone
+             * -  password
+             * -  imgCode
+             * -  submit
+             * -  imgCodeButton
+             */
+
+            
+            // 判断参数并提示
+            (() => {
+                let mustParam = ['phone','password','imgCode','submit','imgCodeButton','warn']
+                mustParam.forEach(item => {
+                    let mark = false
+                    for(key in param){
+                        if(key == item){
+                            mark = true
+                        }
+                    }
+                    if(!mark){
+                        console.error(`请填写登录${item}的dom`)
+                    }
+                })
+            })(param)
+            
+            // 获取图片验证码
+            let imgCode = (formToken) => {
+                $(param.imgCodeButton).css('background-image',`url(${Api.imgCode}?formtoken=${formToken}&t=${Math.random()})`)
+            }
+            // 校验图片验证码
+            let checkKaptcha = (imgCode) => {
+                let pro = new Promise((resolve,reject) => {
+                    com.ajax({
+                        url: Api.checkKaptcha,
+                        contentType: "application/json",
+                        type: "get",
+                        headers:{
+                            "Accept": "application/json;charset=UTF-8",
+                            "clientId": "XXD_INTEGRATION_PLATFORM",
+                            "clientTime": new Date().getTime()
+                        },
+                        data: {
+                            kaptchaCode: imgCode,
+                            del: true
+                        },
+                        success: (res) => {
+                            if(res.code == rcode && res.data.code == 0){
+                                resolve(res.data)
+                            }
+                            else{
+                                reject(res.data)
+                            }
+                        },
+                        error: (res) => {
+                            reject(res)
+                        }
+                    })
+                })
+                return pro
+            }
+            // 登录
+            let loginWay = (userData_1) => {
+                let formData = {
+                    data: {
+                        userName: userData_1.phone,
+                        phone:    userData_1.phone,
+                        password: $.md5($.md5(userData_1.password)),
+                        kaptcha:  userData_1.imgCode
+                    }
+                }
+                let pro = new Promise((resolve,reject) => {
+                    com.ajax({
+                        url: Api.login,
+                        contentType: "application/json",
+                        type: "post",
+                        headers:{
+                            "Accept": "application/json;charset=UTF-8",
+                            "clientId": "XXD_INTEGRATION_PLATFORM",
+                            "clientTime": new Date().getTime()
+                        },
+                        data: JSON.stringify(formData),
+                        success: (res) => {
+                            if(res.code == rcode && res.data.data){
+                                resolve(res.data)
+                            }
+                            else{
+                                reject(res.data)
+                            }
+                        },
+                        error: (res) => {
+                            reject(res)
+                        }
+                    })
+                })
+                return pro
+            }
+
+            // 初始获取formToken
+            getFormToken().then(data => {formToken_log = data},data => {formToken_log = data})
+            // 初始化展示图片验证码
+            imgCode(formToken_reg)
+            // 获取电话
+            getValue(userData_1,param.phone,'phone')
+            // 获取密码
+            getValue(userData_1,param.password,'password')
+            // 获取图片验证码
+            getValue(userData_1,param.imgCode,'imgCode')
+
+            /**
+             * 点击事件
+             */
+            // 点击获取图片验证码
+            $(param.imgCodeButton).click(() => {
+                getFormToken().then(
+                    formToken => {
+                        imgCode(formToken)
+                    },
+                    formToken => {
+                        imgCode(formToken)
+                    }
+                )
+            })
+            // 点击提交用户数据
+            $(param.submit).click(() => {
+                checkKaptcha(userData_1.imgCode).then(
+                    () => {
+                        loginWay(userData_1).then(
+                            (res) => {
+                                function setCookie(cname, cvalue, exdays) {
+                                    var d = new Date();
+                                    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+                                    var expires = "expires="+d.toUTCString();
+                                    document.cookie = cname + "=" + cvalue + "; " + expires;
+                                }
+                                setCookie('userToken',res.data,1)//把tkoen存起来
+                                $(param.warn).removeClass('dishidden').text('登录成功')
+                                // 成功回调
+                                if(param.success && typeof(param.success) == 'function'){
+                                    param.success(res)
+                                }
+                            },
+                            (error) => {
+                                $(param.warn).removeClass('dishidden').text(error.message)
+                                // 失败回调
+                                if(param.fail && typeof(param.fail) == 'function'){
+                                    param.fail(error)
+                                }
+                            }
+                        )
+                    }
+                    ,
+                    () => {
+                        $(param.warn).removeClass('dishidden').text('图片验证码错误')
+                    }
+                )
+            })
+
+        }
+
+        return {register,login}
     }
     return user()
 })
